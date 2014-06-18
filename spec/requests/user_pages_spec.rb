4e7ad1e9b684233f572c4915ae6f01ccc770773d
Jesse Hu <huyouare@gmail.com>
2014-06-18 01:35:43 -0700

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -37,11 +37,10 @@ describe "User pages" do
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
-    end
 
-    it "should create a user" do
-      expect { click_button submit }.to change(User, :count).by(1)
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
     end
-
   end
 end
