847866a0c4b4454980d3ec29d91d2f4a1a3d58f4
Jesse Hu <huyouare@gmail.com>
2014-06-18 18:09:03 -0700

    Finish Chapter 7 exercises

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,5 +21,6 @@ class User < ActiveRecord::Base
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
+  after_validation { self.errors.messages.delete(:password_digest) }
   validates :password_confirmation, presence: true
 end
